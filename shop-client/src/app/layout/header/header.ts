18489b2c491f785f37f3f4a363972dd0112f0615
hadi <hadi.deris.h82@gmail.com>
2026-07-10 12:05:19 +0330

    feat: implement storefront header

--- shop-client/src/app/layout/header/header.ts
+++ shop-client/src/app/layout/header/header.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, MatButtonModule, MatIconModule],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
