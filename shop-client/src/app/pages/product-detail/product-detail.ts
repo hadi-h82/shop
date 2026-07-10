cb0381ba54ad444ae9b4bdb58b2c583a65c1e8df
hadi <hadi.deris.h82@gmail.com>
2026-07-10 14:48:59 +0330

    polish product detail layout

--- shop-client/src/app/pages/product-detail/product-detail.ts
+++ shop-client/src/app/pages/product-detail/product-detail.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { DecimalPipe } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+import { MOCK_PRODUCTS } from '../../core/mock-data/products.mock';
 
 @Component({
   selector: 'app-product-detail',
-  imports: [],
+  imports: [DecimalPipe, RouterLink, MatButtonModule, MatIconModule],
   templateUrl: './product-detail.html',
-  styleUrl: './product-detail.scss',
+  styleUrl: './product-detail.scss'
 })
-export class ProductDetail {}
+export class ProductDetail {
+  private readonly route = inject(ActivatedRoute);
+
+  readonly slug = this.route.snapshot.paramMap.get('id');
+
+  readonly product = computed(() =>
+    MOCK_PRODUCTS.find(product => product.slug === this.slug)
+  );
+}
