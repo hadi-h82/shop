afdca4135c20c4a7b140f367eb83bd7df0818194
hadi <hadi.deris.h82@gmail.com>
2026-07-10 14:31:58 +0330

    feat: add products listing page

--- shop-client/src/app/pages/products/products.ts
+++ shop-client/src/app/pages/products/products.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { MOCK_PRODUCTS } from '../../core/mock-data/products.mock';
+import { ProductCard } from '../../shared/ui/product-card/product-card';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [ProductCard],
   templateUrl: './products.html',
-  styleUrl: './products.scss',
+  styleUrl: './products.scss'
 })
-export class Products {}
+export class Products {
+  readonly products = MOCK_PRODUCTS;
+}
