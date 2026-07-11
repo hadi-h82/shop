bc11b01fa0362c7fd41901feed8eadfe2b1bc789
hadi <hadi.deris.h82@gmail.com>
2026-07-11 18:33:15 +0330

    feat: implement forgot password form

--- shop-client/src/app/pages/auth/forgot-password/forgot-password.ts
+++ shop-client/src/app/pages/auth/forgot-password/forgot-password.ts
@@ -1,9 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  ReactiveFormsModule,
+  Validators
+} from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    RouterLink,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
+  ],
   templateUrl: './forgot-password.html',
-  styleUrl: './forgot-password.scss',
+  styleUrl: './forgot-password.scss'
 })
-export class ForgotPassword {}
+export class ForgotPassword {
+  private readonly formBuilder = inject(FormBuilder);
+
+  submitted = false;
+
+  readonly forgotPasswordForm = this.formBuilder.nonNullable.group({
+    email: ['', [Validators.required, Validators.email]]
+  });
+
+  submit(): void {
+    if (this.forgotPasswordForm.invalid) {
+      this.forgotPasswordForm.markAllAsTouched();
+      return;
+    }
+
+    this.submitted = true;
+
+    console.log(this.forgotPasswordForm.getRawValue());
+  }
+}
