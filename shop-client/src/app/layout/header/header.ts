6bbb13eae11a1eecaa356fb60eecd6f17b0befe4
hadi deriszadeh <hadi.deris.h82@gmail.com>
2026-07-12 11:09:07 +0330

    feat: update header layout with improved logo, search, navigation, and action styles

--- shop-client/src/app/layout/header/header.ts
+++ shop-client/src/app/layout/header/header.ts
@@ -1,16 +1,27 @@
-import { Component,inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import {
+  RouterLink,
+  RouterLinkActive
+} from '@angular/router'; 
+
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+
 import { CartService } from '../../core/services/cart';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, MatButtonModule, MatIconModule],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
 export class Header {
+  readonly cart = inject(CartService);
 
-   readonly cart = inject(CartService);
+
 }
