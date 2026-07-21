b81c08d8a0c4ad809016d964f7d95dfec39f81d7
hadi <hadi.deris.h82@gmail.com>
2026-07-21 22:05:03 +0330

    feat: enhance product card with preview title and style adjustments

--- shop-client/src/app/shared/ui/product-card/product-card.ts
+++ shop-client/src/app/shared/ui/product-card/product-card.ts
@@ -1,17 +1,13 @@
 import { Component, input } from '@angular/core';
-import { DecimalPipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
 import { Product } from '../../../core/models/product.model';
-import { inject } from '@angular/core';
-import { CartService } from '../../../core/services/cart';
 
 @Component({
   selector: 'app-product-card',
 imports: [
-  DecimalPipe,
   RouterLink,
   MatButtonModule,
   MatIconModule
@@ -21,16 +17,4 @@ imports: [
 })
 export class ProductCard {
   product = input.required<Product>();
-
-  private readonly cart = inject(CartService);
-
-addToCart(): void {
-  const item = this.product();
-
-  if (!item.isAvailable || item.price <= 0) {
-    return;
-  }
-
-  this.cart.add(item);
-}
 }
