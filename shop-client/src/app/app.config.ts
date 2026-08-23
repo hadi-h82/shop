7c704913a10614f63df9d84e12cc6dc7f51693a7
hadi deriszadeh <hadi.deris.h82@gmail.com>
2026-08-23 12:42:32 +0330

    feat: reorganize imports and enhance app configuration

--- shop-client/src/app/app.config.ts
+++ shop-client/src/app/app.config.ts
@@ -1,18 +1,23 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { provideClientHydration } from '@angular/platform-browser';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+
     provideRouter(
       routes,
       withInMemoryScrolling({
         scrollPositionRestoration: 'top'
       })
     ),
+
+    provideHttpClient(),
+
     provideClientHydration()
   ]
 };
