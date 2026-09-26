ac2992344b6ce5d92bfd7d2b0131e056ed3f56ae
hadi <hadi.deris.h82@gmail.com>
2026-09-26 23:40:23 +0330

    feat: add Dockerfile and .dockerignore for containerization support refactor: streamline angular.json configurations and remove unused routes

--- shop-client/src/app/app.routes.server.ts
+++ shop-client/src/app/app.routes.server.ts
@@ -1,40 +1,12 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
-import { MOCK_CATEGORIES } from './core/mock-data/categories.mock';
-import { MOCK_PRODUCTS } from './core/mock-data/products.mock';
-
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'products/:id',
-    renderMode: RenderMode.Prerender,
-
-    async getPrerenderParams() {
-      return MOCK_PRODUCTS.map((product) => ({
-        id: product.slug,
-      }));
-    },
-  },
-
-  {
-    path: 'categories/:slug',
-    renderMode: RenderMode.Prerender,
-
-    async getPrerenderParams() {
-      return MOCK_CATEGORIES
-        .filter((category) => category.isActive)
-        .map((category) => ({
-          slug: category.slug,
-        }));
-    },
-  },
-
-  {
     path: 'admin/**',
-    renderMode: RenderMode.Client,
+    renderMode: RenderMode.Client
   },
-
   {
     path: '**',
-    renderMode: RenderMode.Prerender,
-  },
+    renderMode: RenderMode.Server
+  }
 ];
